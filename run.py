9295b554166c0f866f8348b3985cede1efc27aa3
Adam Gilroy <adamgilroy22@gmail.com>
2022-10-26 15:00:32 +0000

    Add game_menu function

--- run.py
+++ run.py
@@ -1,3 +1,7 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def game_menu():
+    print("Press 1 To Start Game")
+    print("Press 2 To Select Difficulty")
+    print("Press 3 To View Rules")
+
+    selection = input("What would you like to do?\n")
+    return selection
