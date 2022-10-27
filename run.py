566c479f80d55d1cebdacd6a94c0ed85101fc6d6
Adam Gilroy <adamgilroy22@gmail.com>
2022-10-27 11:54:06 +0000

    Add while loop for game menu

--- run.py
+++ run.py
@@ -3,5 +3,17 @@ def game_menu():
     print("Press 2 To Select Difficulty")
     print("Press 3 To View Rules")
 
-    selection = input("What would you like to do?\n")
-    return selection
+    menu_selection = False
+    while not menu_selection:
+        selection = input("What would you like to do?\n")
+        if selection == 1:
+            menu_selection = True
+
+        elif selection == 2:
+            menu_selection = True
+
+        elif selection == 3:
+            menu_selection = True
+
+        else:
+            print("Select 1, 2 or 3")
